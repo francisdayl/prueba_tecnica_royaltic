30118c17fe356e635344fb1dc66c5c68f68d7eda
francisdayl <francisdaylol@gmail.com>
2024-09-08 03:59:00 -0500

    set starting point for frontend

---
+++ frontend/src/app/pages/admin-category-detail/admin-category-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-category-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-category-detail.component.html',
+  styleUrl: './admin-category-detail.component.css'
+})
+export class AdminCategoryDetailComponent {
+
+}
